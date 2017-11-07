aa0fc62e6612cdb63445b50f9e6cc504ec88d25d
Kara Cavanaugh <karacav@msn.com>
2017-11-07 14:52:07 -0600

    debugging step 1

--- stage-1/src/components/Home/Home.js
+++ stage-1/src/components/Home/Home.js
@@ -1,11 +1,13 @@
+import React, { Component } from 'react';
 import './Home.css';
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="home-landing"></div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className="home-landing" />;
+  }
 }
 
 export default Home;
